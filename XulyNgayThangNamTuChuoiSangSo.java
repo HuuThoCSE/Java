3bea20e0525f4e3f7c2e3e6633e10a468c4da64c
Hữu Thọ CS <89997693+huuthocs@users.noreply.github.com>
2022-11-24 01:53:09 +0000

    8h52am24-11-22

--- XulyNgayThangNamTuChuoiSangSo.java
+++ XulyNgayThangNamTuChuoiSangSo.java
@@ -1,1 +1,9 @@
-hello
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
+public class StringToDateExample1 {  
+public static void main(String[] args)throws Exception {  
+    String sDate1="1/1/2002";  
+    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);  
+    System.out.println(sDate1+" => "+date1); 
+    }  
+}  
